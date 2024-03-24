0204cf599b7b179bb4a26b8725a25ac61dcd8c8e
erfansalman <estaheri101@gmail.com>
2024-03-24 18:41:16 +0330

    fixed input for role

--- src/Student.java
+++ src/Student.java
@@ -1,6 +1,6 @@
 public class Student extends Person {
     public Student(String personId, String password, String name, String lastName, String nationalCode,
-                  String birthdate, String address, String role) {
-        super(personId, password, name, lastName, nationalCode, birthdate, address, role);
+                  String birthdate, String address) {
+        super(personId, password, name, lastName, nationalCode, birthdate, address, "student");
     }
 }
