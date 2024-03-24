2e30aa8c56c86668aa40a6667139b9ac79624a79
erfansalman <estaheri101@gmail.com>
2024-03-24 22:25:13 +0330

    added print for results

--- src/TestParser.java
+++ src/TestParser.java
@@ -2,13 +2,13 @@ public class TestParser {
     public void parse(String[] command) {
         Center.initialAdmin();
         if (command[0].equals("add-library")) {
-            Center.addLibrary(command);
+            System.out.println(Center.addLibrary(command));
         } else if (command[0].equals("add-category")) {
-            Center.addCategory(command);
+            System.out.println(Center.addCategory(command));
         } else if (command[0].equals("add-student") || command[0].equals("add-staff") || command[0].equals("add-manager")) {
-            Center.addPerson(command);
+            System.out.println(Center.addPerson(command));
         } else if (command[0].equals("remove-user")) {
-            Center.removePerson(command);
+            System.out.println(Center.removePerson(command));
         }
     }
 }
