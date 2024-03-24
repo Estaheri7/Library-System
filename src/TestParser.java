8ee1f12a1400dfc1ff4f25ad819e83a5cbb8fd30
erfansalman <estaheri101@gmail.com>
2024-03-24 21:26:44 +0330

    added add and remove methods

--- src/TestParser.java
+++ src/TestParser.java
@@ -1,5 +1,14 @@
 public class TestParser {
     public void parse(String[] command) {
-
+        Center.initialAdmin();
+        if (command[0].equals("add-library")) {
+            Center.addLibrary(command);
+        } else if (command[0].equals("add-category")) {
+            Center.addCategory(command);
+        } else if (command[0].equals("add-student") || command[0].equals("add-staff") || command[0].equals("add-manager")) {
+            Center.addPerson(command);
+        } else if (command[0].equals("remove-user")) {
+            Center.removePerson(command);
+        }
     }
 }
