92808e37061eb5122cf5dac010183e729035bc34
erfansalman <estaheri101@gmail.com>
2024-03-24 19:16:58 +0330

    add method to check object existance

--- src/Center.java
+++ src/Center.java
@@ -4,4 +4,16 @@ public class Center {
     private final static HashMap<String, Person> persons = new HashMap<>();
     private final static HashMap<String, Library> libraries = new HashMap<>();
     private final static HashMap<String, Category> categories = new HashMap<>();
+
+    private static boolean libraryExists(String key) {
+        return libraries.containsKey(key);
+    }
+
+    private static boolean categoryExists(String key) {
+        return categories.containsKey(key);
+    }
+
+    private static boolean personExists(String key) {
+        return persons.containsKey(key);
+    }
 }
