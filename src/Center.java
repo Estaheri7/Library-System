1db99958e031f87ae4dd326c75a6937b4a5ccf6e
erfansalman <estaheri101@gmail.com>
2024-03-24 19:59:02 +0330

    added addLibrary method

--- src/Center.java
+++ src/Center.java
@@ -6,6 +6,36 @@ public class Center {
     private final static HashMap<String, Library> libraries = new HashMap<>();
     private final static HashMap<String, Category> categories = new HashMap<>();
 
+    public static String addLibrary(String[] args) {
+        String personId = args[1];
+        String personPassword = args[2];
+        String libraryId = args[3];
+        String name = args[4];
+        String year = args[5];
+        int numberOfTables = Integer.parseInt(args[6]);
+        String address = args[7];
+
+        if (!personExists(personId)) {
+            return "not-found";
+        }
+
+        if (libraryExists(libraryId)) {
+            return "duplicate-id";
+        }
+
+        if (!describeRole(personId).equals("admin")) {
+            return "permission-denied";
+        }
+
+        if (!isCorrectPassword(personId, personPassword)) {
+            return "invalid-pass";
+        }
+
+        Library library = new Library(libraryId, name, year, numberOfTables, address);
+        libraries.put(libraryId, library);
+        return "success";
+    }
+
     private static boolean libraryExists(String key) {
         return libraries.containsKey(key);
     }
@@ -22,7 +52,7 @@ public class Center {
         return persons.get(personId).getRole();
     }
 
-    private static boolean checkPassword(String personId, String password) {
+    private static boolean isCorrectPassword(String personId, String password) {
         return persons.get(personId).getPassword().equals(password);
     }
 }
