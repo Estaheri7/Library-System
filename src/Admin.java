d67cf3497ffa796f2d0a28aa6820fc48f106514f
erfansalman <estaheri101@gmail.com>
2024-03-24 18:43:10 +0330

    add constructor

--- src/Admin.java
+++ src/Admin.java
@@ -2,4 +2,9 @@ public class Admin {
     private String username;
     private String password;
     private final String role = "admin";
+
+    public Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
