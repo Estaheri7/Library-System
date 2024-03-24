b18db95e66935bc76879bb676035f3a267daa517
erfansalman <estaheri101@gmail.com>
2024-03-24 18:56:34 +0330

    add fields

--- src/Center.java
+++ src/Center.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+
 public class Center {
-    
+    private final static HashMap<String, Person> persons = new HashMap<>();
+    private final static HashMap<String, Library> libraries = new HashMap<>();
+    private final static HashMap<String, Category> categories = new HashMap<>();
 }
