e0e1cf1099239374ac46665a5ac8bdd62976e46a
erfansalman <estaheri101@gmail.com>
2024-03-30 18:33:27 +0330

    add command conditions

--- src/TestParser.java
+++ src/TestParser.java
@@ -9,6 +9,14 @@ public class TestParser {
             System.out.println(Center.addPerson(command));
         } else if (command[0].equals("remove-user")) {
             System.out.println(Center.removePerson(command));
+        } else if (command[0].equals("add-book")) {
+            System.out.printf(Center.addBook(command));
+        } else if (command[0].equals("add-thesis")) {
+            System.out.printf(Center.addThesis(command));
+        } else if (command[0].equals("add-ganjineh-book")) {
+            System.out.println(Center.addGanjineh(command));
+        } else if (command[0].equals("add-seeling-book")) {
+            System.out.println(Center.addSellingBook(command));
         }
     }
 }
