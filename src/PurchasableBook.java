67170b23fb7c495dccdbfa8be03ed39b78ac9556
erfansalman <estaheri101@gmail.com>
2024-03-30 17:43:21 +0330

    add consturctor

--- src/PurchasableBook.java
+++ src/PurchasableBook.java
@@ -1,5 +1,13 @@
-public class PurchasableBook {
+public class PurchasableBook extends Item {
     private String publisher;
     private String price;
     private String discount;
+
+    public PurchasableBook(String itemId, String title, String authorName, String publisher, String year,
+                           int copies, String price, String discount, String categoryId, String libraryId) {
+        super(itemId, title, authorName, year, copies, categoryId, libraryId);
+        this.publisher = publisher;
+        this.price = price;
+        this.discount = discount;
+    }
 }
--- src/TreasureBook.java
+++ src/TreasureBook.java
@@ -1,5 +1,11 @@
-public class TreasureBook {
+public class TreasureBook extends Item {
     private String publisher;
     private String donateName;
 
+    public TreasureBook(String itemId, String title, String authorName, String publisher, String year,
+                        String donateName, String categoryId, String libraryId) {
+        super(itemId, title, authorName, year, 1, categoryId, libraryId);
+        this.publisher = publisher;
+        this.donateName = donateName;
+    }
 }
