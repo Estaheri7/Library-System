3f8ebaac8bed997d5bd154df32f396a0023416a4
erfansalman <estaheri101@gmail.com>
2024-03-30 17:49:18 +0330

    add new field for items

--- src/Library.java
+++ src/Library.java
@@ -1,9 +1,13 @@
+import java.util.HashMap;
+
 public class Library {
     private String libraryId;
     private String name;
     private String establishedYear;
     private int numberOfTables;
     private String address;
+
+    private HashMap<String, Item> items = new HashMap<>();
 
     public Library(String libraryId, String name, String establishedYear, int numberOfTables, String address) {
         this.libraryId = libraryId;
