db93f7595f65d790f249ddf86c5637cb834c2547
erfansalman <estaheri101@gmail.com>
2024-04-01 17:31:40 +0330

    add fields and constructor

--- src/Borrow.java
+++ src/Borrow.java
@@ -1,3 +1,16 @@
-public class Borrow {
+import java.time.LocalDateTime;
 
+public class Borrow {
+    private String userId;
+    private String libraryId;
+    private String itemId;
+    private LocalDateTime borrowTime;
+
+    public Borrow(String userId, String libraryId, String itemId, String date, String clock) {
+        this.userId = userId;
+        this.libraryId = libraryId;
+        this.itemId = itemId;
+        String fullDate = date + "T" + clock;
+        this.borrowTime = LocalDateTime.parse(fullDate);
+    }
 }
