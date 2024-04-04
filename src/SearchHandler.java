1bf2b1446fe2169535575692e29e2ae7046d0ced
erfansalman <estaheri101@gmail.com>
2024-04-04 20:37:19 +0330

    add method for searching in Books

--- src/SearchHandler.java
+++ src/SearchHandler.java
@@ -1,7 +1,40 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class SearchHandler {
     private String searchKey;
+    private ArrayList<String> books = new ArrayList<>();
 
     public SearchHandler(String searchKey) {
         this.searchKey = searchKey;
+    }
+
+    public StringBuilder searchBook() {
+        for (Library library : Center.getLibraries().values()) {
+            for (Item item : library.getItems().values()) {
+                if (item.search(this.searchKey)) {
+                    this.books.add(item.getItemId());
+                }
+            }
+        }
+
+        return returnResults();
+    }
+
+    private StringBuilder returnResults() {
+        if (this.books.size() == 0) {
+            return new StringBuilder("not-found");
+        }
+
+        Collections.sort(this.books);
+        StringBuilder bookIds = new StringBuilder();
+
+        for (String id : this.books) {
+            bookIds.append(id);
+            bookIds.append("|");
+        }
+
+        bookIds.deleteCharAt(bookIds.length() - 1);
+        return bookIds;
     }
 }
