d291e4b4b4d5fe75ab707190f27449e46b4aea42
erfansalman <estaheri101@gmail.com>
2024-04-06 18:08:38 +0330

    add constructor

--- src/ReportHandler.java
+++ src/ReportHandler.java
@@ -1,3 +1,9 @@
 public class ReportHandler {
-    
+    private String personId;
+    private String password;
+
+    public ReportHandler(String personId, String password) {
+        this.personId = personId;
+        this.password = password;
+    }
 }
