d3902f7ae15bdcbcc2c0c279cbe55b96d1730e76
erfansalman <estaheri101@gmail.com>
2024-04-06 19:01:44 +0330

    add categoryReport

--- src/ReportHandler.java
+++ src/ReportHandler.java
@@ -1,3 +1,5 @@
+import java.util.HashSet;
+
 public class ReportHandler {
     private String personId;
     private String password;
@@ -5,5 +7,37 @@ public class ReportHandler {
     public ReportHandler(String personId, String password) {
         this.personId = personId;
         this.password = password;
+    }
+
+    public String categoryReport(String categoryId, String libraryId) {
+        if (!Center.libraryExists(libraryId) || !Center.personExists(this.personId) ||
+            !Center.categoryExists(categoryId)) {
+            return "not-found";
+        }
+
+        if (!Center.isLibraryManager(this.personId, libraryId)) {
+            return "permission-denied";
+        }
+
+        if (!Center.isCorrectPassword(this.personId, this.password)) {
+            return "invalid-pass";
+        }
+
+        Library library = Center.getLibraries().get(libraryId);
+        HashSet<String> categories = new HashSet<>();
+        categories.add(categoryId);
+        if (categoryId.equals("null")) {
+            return library.categoryReport(categories);
+        }
+
+        for (Category category : Center.getCategories().values()) {
+            for (String superCategory : categories) {
+                if (category.getSuperCategory().equals(superCategory)) {
+                    categories.add(category.getCategoryId());
+                }
+            }
+        }
+
+        return library.categoryReport(categories);
     }
 }
